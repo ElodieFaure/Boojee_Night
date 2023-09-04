5a9dedb1e818971ebae1d37110c462fd429300c6
Sophia Benani <sophiabenani@gmail.com>
2023-09-04 11:06:11 +0200

    revue back client et bar

---
+++ app/javascript/controllers/tooltip_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="tooltip"
+export default class extends Controller {
+  connect() {
+    new bootstrap.Tooltip(this.element)
+  }
+}
