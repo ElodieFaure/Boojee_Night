70257604605a32c9ff9412e36dd5ddb1e2a5c5ca
elodie faure <efaure33@wanadoo.fr>
2023-09-05 10:45:07 +0200

    de tout v2

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module BoojeeNight
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.i18n.default_locale = :fr
+
 
     # Configuration for the application, engines, and railties goes here.
     #
